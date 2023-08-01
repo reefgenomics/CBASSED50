e57b1b19ff9027b632b25909bfb0d9043a7dc94b
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-08-01 14:53:25 +0200

    update doc and improved tests

--- R/preprocess-and-validate-data.R
+++ R/preprocess-and-validate-data.R
@@ -82,70 +82,128 @@ dataset_has_mandatory_columns <- function(dataset) {
 #'                    PAM = c("0.4", "0.6", "0.7"))
 #'
 #' # Convert columns in the dataset
-#' modified_data <- check_and_convert_columns(data)
+#' modified_data <- convert_columns(data)
 #'
 #' # The 'Site', 'Condition', 'Species', and 'Genotype' columns are now factors,
 #' # and 'Temperature' and 'PAM' columns are now numeric in the modified_data.
-check_and_convert_columns <- function(dataset) {
+convert_columns <- function(dataset) {
   # Check if the column 'Site' is not a factor, then convert to factor
   if (!is.factor(dataset$Site)) {
+    rlog::log_info("`Site` column was converted to factor.")
     dataset$Site <- as.factor(dataset$Site)
   }
   # Check if the column 'Condition' is not a factor, then convert to factor
   if (!is.factor(dataset$Condition)) {
+    rlog::log_info("`Condition` column was converted to factor.")
     dataset$Condition <- as.factor(dataset$Condition)
   }
   # Check if the column 'Species' is not a factor, then convert to factor
   if (!is.factor(dataset$Species)) {
+    rlog::log_info("`Species` column was converted to factor.")
     dataset$Species <- as.factor(dataset$Species)
   }
   # Check if the column 'Genotype' is not a factor, then convert to factor
   if (!is.factor(dataset$Genotype)) {
+    rlog::log_info("`Genotype` column was converted to factor.")
     dataset$Genotype <- as.factor(dataset$Genotype)
   }
   # Check if the column 'Temperature' is not numeric, then convert to numeric
   if (!is.numeric(dataset$Temperature)) {
+    rlog::log_info("`Temperature` column was converted to numeric.")
     dataset$Temperature <- as.numeric(dataset$Temperature)
   }
   # Check if the column 'PAM' is not numeric, then convert to numeric
   if (!is.numeric(dataset$PAM)) {
+    rlog::log_info("`PAM` column was converted to numeric.")
     dataset$PAM <- as.numeric(dataset$PAM)
   }
   return(dataset)
 }
 
-#' Process and Validate CBASS Dataset
+
+#' Check if the dataset contains enough unique temperature values
 #'
-#' This function processes and validates a dataset to ensure it contains all the mandatory columns required for further processing.
+#' @param dataset The input dataset containing the 'Temperature' column to be analyzed.
+#'        The 'Temperature' column should be numeric representing
+#'        temperature values.
+#'
+#' @return Logical value (TRUE or FALSE) indicating whether there are enough unique
+#'         temperature values (at least 4) in the dataset.
+#'
+#' @examples
+#' data <- data.frame(Temperature = c(25, 30, 25, 35, 28, 28))
+#' check_enough_unique_temperatures_values(data)
+#' # Output: TRUE
+check_enough_unique_temperature_values <- function(dataset) {
+  if (length(unique(dataset$Temperature)) < 4) {
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
+#' Preprocesses the data by converting and checking column data types.
+#'
+#' This function preprocesses the input data by performing checks on column data types
+#' and converting them if necessary. It ensures that the dataset meets certain requirements
+#' before further analysis or modeling.
+#'
+#' @param dataset A data frame containing the dataset to be preprocessed.
+#'
+#' @return A preprocessed data frame with converted and validated column data types.
+#'
+#' @export
+#'
+#' @examples
+#' # Load a sample dataset
+#' data("cbass_dataset")
+#' # Preprocess the dataset
+#' preprocessed_data <- preprocess_data(cbass_dataset)
+preprocess_dataset <- function(dataset) {
+  dataset <- convert_columns(dataset)
+  return(dataset)
+}
+
+#' Validate CBASS Dataset
+#'
+#' This function validates a dataset to ensure it contains all the mandatory columns required for further processing.
 #' If any mandatory columns are missing, it raises an error with the list of missing columns.
-#' Additionally, the function checks and converts the columns in the dataset.
 #'
 #' @param dataset A data frame representing the CBASS dataset to be processed and validated.
 #'
 #' @return A processed and validated CBASS dataset with appropriate data types for its columns.
 #'
-#' @seealso \code{\link{dataset_has_mandatory_columns}}, \code{\link{check_and_convert_columns}}
+#' @seealso \code{\link{dataset_has_mandatory_columns}}, \code{\link{convert_columns}}, \code{\link{check_enough_unique_temperature_values}}
 #'
-#' @import rlog
+#' @importFrom rlog log_info log_error
+#' @importFrom glue glue
 #'
 #' @examples
-#' data(cbass_dataset) # Assuming a dataset named 'cbass_dataset' is available in the environment
+#' # Assuming a dataset named 'cbass_dataset' is available in the environment
+#' data(cbass_dataset)
 #' processed_dataset <- process_and_validate_cbass_dataset(cbass_dataset)
 #'
 #' @export
-process_and_validate_cbass_dataset <- function(dataset) {
-  rlog::log_info("Check if the dataset has all mandatory columns")
+validate_cbass_dataset <- function(dataset) {
+  rlog::log_info("Checking if the dataset has all mandatory columns...")
   if (!dataset_has_mandatory_columns(dataset)) {
     missing_columns <- setdiff(mandatory_columns(), names(dataset))
     stop(rlog::log_error(
       paste(
-        "Error: Dataset is missing mandatory columns:",
+        "Dataset is missing mandatory columns:",
         paste(missing_columns, collapse = ", ")
       )
     ))
   }
-  rlog::log_info("Convert and check columns datatypes in Dataset")
-  dataset <- check_and_convert_columns(dataset)
-  rlog::log_info("Your dataset passes all check!")
-  return(dataset)
+  rlog::log_info("Checking if the dataset has enough temperature values...")
+  if (!check_enough_unique_temperature_values(dataset)) {
+    stop(rlog::log_error(
+      glue::glue(
+        "Dataset does not have enough unique temperature values: ",
+        "{length(unique(dataset$Temperature))}. ",
+        "There should be at least 4 unique values!"
+      )
+    ))
+  }
+  rlog::log_info("Your dataset passes all checks!")
+  return(TRUE)
 }
---
+++ tests/testthat/test-preprocess-and-validate-data.R.R
@@ -0,0 +1,111 @@
+# Test for the `mandatory_columns()` function
+test_that("mandatory_columns returns the correct vector of mandatory columns", {
+  expected <- c(
+    "Date",
+    "Country",
+    "Site",
+    "Condition",
+    "Species",
+    "Genotype",
+    "Temperature",
+    "PAM"
+  )
+  result <- mandatory_columns()
+  expect_equal(result, expected)
+})
+
+# Tests for the `dataset_has_mandatory_columns()` function
+test_that("dataset_has_mandatory_columns returns TRUE when all mandatory columns are present", {
+  sample_data <- data.frame(
+    Date = c("2023-07-31", "2023-08-01", "2023-08-02"),
+    Country = c("Country A", "Country B", "Country C"),
+    Site = c("Site X", "Site Y", "Site Z"),
+    Condition = c("Condition 1", "Condition 2", "Condition 3"),
+    Species = c("Species 1", "Species 2", "Species 3"),
+    Genotype = c("Genotype A", "Genotype B", "Genotype C"),
+    Temperature = c(30.2, 31.5, 29.8),
+    PAM = c(0.5, 0.6, 0.8)
+  )
+
+  result <- dataset_has_mandatory_columns(sample_data)
+  expect_true(result)
+})
+
+test_that("dataset_has_mandatory_columns returns FALSE when any mandatory column is missing", {
+  missing_columns_data <- data.frame(
+    Label = c("A", "B", "C"),
+    Date = c("2023-07-31", "2023-08-01", "2023-08-02")
+  )
+
+  result <- dataset_has_mandatory_columns(missing_columns_data)
+  expect_false(result)
+})
+
+# Test for the `convert_columns()` function
+test_that("convert_columns converts columns to the correct types", {
+  data <- data.frame(
+    Site = c("A", "B", "C"),
+    Condition = c("Control", "Experimental", "Control"),
+    Species = c("Species1", "Species2", "Species1"),
+    Genotype = c("GenotypeA", "GenotypeB", "GenotypeA"),
+    Temperature = c("25", "28", "30"),
+    PAM = c("0.4", "0.6", "0.7")
+  )
+
+  modified_data <- convert_columns(data)
+
+  # Assert that the 'Site', 'Condition', 'Species', and 'Genotype' columns are factors
+  expect_s3_class(modified_data$Site, "factor")
+  expect_s3_class(modified_data$Condition, "factor")
+  expect_s3_class(modified_data$Species, "factor")
+  expect_s3_class(modified_data$Genotype, "factor")
+
+  # Assert that the 'Temperature' and 'PAM' columns are numeric
+  expect_type(modified_data$Temperature, "double")
+  expect_type(modified_data$PAM, "double")
+})
+
+# Tests for the `check_enough_unique_temperature_values` function
+test_that("check_enough_unique_temperature_values returns TRUE when there are enough unique values", {
+  data <- data.frame(Temperature = c(25, 30, 25, 35, 28, 28))
+  result <- check_enough_unique_temperature_values(data)
+  expect_true(result)
+})
+
+test_that("check_enough_unique_temperature_values returns FALSE when there are not enough unique values", {
+  data <- data.frame(Temperature = c(25, 25, 25, 25))
+  result <- check_enough_unique_temperature_values(data)
+  expect_false(result)
+})
+
+# Tests for `validate_cbass_dataset` function
+test_that("validate_cbass_dataset throws an error when mandatory columns are missing", {
+  missing_columns_data <- data.frame(
+    Label = c("A", "B", "C"),
+    Date = c("2023-07-31", "2023-08-01", "2023-08-02")
+  )
+
+  expect_error(validate_cbass_dataset(missing_columns_data))
+})
+
+test_that("validate_cbass_dataset throws an error when there are not enough unique temperature values", {
+  data <- data.frame(Temperature = c(25, 25, 25, 25))
+
+  expect_error(validate_cbass_dataset(data))
+})
+
+test_that("validate_cbass_dataset returns TRUE for a valid dataset", {
+  sample_data <- data.frame(
+    Date = c("2023-07-31", "2023-08-01", "2023-08-02", "2023-09-02"),
+    Country = c("Country A", "Country B", "Country C", "Country A"),
+    Site = c("Site X", "Site Y", "Site Z", "Site Z"),
+    Condition = c("Condition 1", "Condition 2", "Condition 3", "Condition 2"),
+    Species = c("Species 1", "Species 2", "Species 3", "Species 1"),
+    Genotype = c("Genotype A", "Genotype B", "Genotype C", "Genotype B"),
+    Temperature = c(30.2, 31.5, 29.8, 29.0),
+    PAM = c(0.5, 0.6, 0.8, 0.7)
+  )
+
+  result <- validate_cbass_dataset(sample_data)
+  expect_true(result)
+})
