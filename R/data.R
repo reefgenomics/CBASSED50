9b4f4a551cc4d45d752cb281e957ce2a067b9b67
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-11-17 17:25:31 +0100

    update the documentation and reading data

--- R/data.R
+++ R/data.R
@@ -1,23 +1,23 @@
 #' CBASS Dataset
 #'
-#' A dataset containing example experimental data.
+#' A dataset containing example simulated experimental data.
 #'
 #' This dataset provides experimental data with various attributes for
 #' demonstration and testing purposes.
 #'
 #' @docType data
 #' @name cbass_dataset
-#' @format A data frame with 300 observations and 9 variables:
+#' @format A data frame with 240 observations and 9 variables:
 #'   \describe{
-#'     \item{Date}{A character vector representing the date of the observation.}
-#'     \item{Country}{A character vector representing the country of the observation.}
-#'     \item{Site}{A character vector representing the site of the observation.}
-#'     \item{Condition}{A character vector representing the treatment or condition of the observation.}
-#'     \item{Species}{A character vector representing the species of the observation.}
-#'     \item{Genotype}{An integer vector representing the genotype of the observation.}
-#'     \item{Temp}{An integer vector representing the temperature of the observation.}
-#'     \item{Timepoint}{An integer vector representing the timepoint of the observation.}
-#'     \item{PAM}{A numeric vector representing the PAM value of the observation.}
+#'     \item{Date}{Date of the observation in YYYYMMDD format.}
+#'     \item{Country}{Country of the observation in 3-letter \href{https://countrycode.org}{ISO country code} format.}
+#'     \item{Site}{Site of the observation, e.g., name of the reef.}
+#'     \item{Condition}{Descriptor to set apart samples from the same species and site, e.g. probiotic treatment vs. control; nursery vs. wild; diseased vs.healthy; can be used to designate experimental treatments besides heat stress. If you don't have any additional conditions besides the heat stress, provide the same condition value, e.g. \bold{Experiment}.}
+#'     \item{Species}{Species of the observation. We recommend providing the name of the coral as accurate as possible, e.g. \emph{Porites lutea} or \emph{Porites sp.}}
+#'     \item{Genotype}{Denotes samples/fragments/nubbins from distinct colonies in a given dataset; we recommend to use integers, i.e. 1, 2, 3, 4, 5, etc.}
+#'     \item{Temperature}{CBASS treatment temperatures; must be ≥ 4 different temperatures; must be integer; e.g. 30, 33, 36, 39. Typical CBASS temperature ranges are average summer mean MMM, MMM+3C, MMM+6C, MMM+9C)}
+#'     \item{Timepoint}{Timepoint of PAM measurements in minutes from start of the thermal cycling profile; typically: 420 (7 hours after start, i.e., after ramping up, heat-hold, ramping down) or 1080 (18 hours after start, i.e. at the end of the CBASS thermal cycling profile); differences in ED50s between timepoints 420 and 1080 may be indicative of resilience/recovery (if 1080 ED50 > 420 ED50) or collapse (if 1080 ED50 < 420 ED50).}
+#'     \item{PAM}{Fv/Fm value of a given sample (format: ≥0 and ≤1, e.g. 0.387); note that technically any continuous variable can be used for ED50 calculation (e.g., coral whitening; black/white pixel intensity; etc.) and be provided in this column.}
 #'   }
 #'
 #' @examples
@@ -26,4 +26,6 @@
 #' head(cbass_dataset)
 #'
 #' @usage data(cbass_dataset)
+#'
+#' @export
 "cbass_dataset"
